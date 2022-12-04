0ed3adcfa1fcd34bc2d2b610d439b8fbf4f0c5c9
Juan Sepulveda <cjfsepu@protonmail.com>
2022-12-04 13:29:27 -0300

    Day 4 part 1

---
+++ Day 04 : Camp Cleanup/index.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+
+const rawInput = fs.readFileSync("input.txt", { encoding: "utf-8" });
+
+const input = rawInput.trim().split("\n");
+
+function part1(input) {
+  let counter = 0;
+  for (const line of input) {
+    const [[pairA1, pairB1], [pairA2, pairB2]] = line
+      .split(",")
+      .map((elf) => elf.split("-").map(Number));
+
+    if (
+      (pairA1 <= pairA2 && pairB1 >= pairB2) ||
+      (pairA1 >= pairA2 && pairB1 <= pairB2)
+    ) {
+      counter++;
+    }
+  }
+  return counter;
+};
+
+console.log(part1(input));
