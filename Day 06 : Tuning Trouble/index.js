5f9b6e6b9c801ffd3ad2d10d758a01dee65030dc
Juan Sepulveda <cjfsepu@protonmail.com>
2022-12-07 14:57:39 -0300

    Day 6 part 2, works need refactor

--- Day 06 : Tuning Trouble/index.js
+++ Day 06 : Tuning Trouble/index.js
@@ -12,4 +12,15 @@ function part1(input) {
   return i;
 }
 
+function part2(input) {
+  let i;
+  for (i = 14; i < input.length - 1; i++) {
+    const chars = input.substring(i - 14, i).split('');
+    const set = new Set(chars);
+    if (set.size === 14) break;
+  }
+  return i;
+}
+
 console.log(part1(rawInput));
+console.log(part2(rawInput));
